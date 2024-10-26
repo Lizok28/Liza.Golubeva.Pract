ad7a857af1bda67475f5b2143fca7e6f21d94a52
Lizok28 <elizavetag924@gmail.com>
2024-10-26 11:07:12 +0300

    repotr

---
+++ PZ_3/PZ_3_2.py
@@ -0,0 +1,28 @@
+#Смоделировать простейший калькулятор,
+# умеющий выполнять 4 основные арифметические операции.
+a = input("Введите первое число: ")     #Обработка искоючений
+while type(a) != float:
+    try:
+        a = float(a)
+    except ValueError:
+        print("Неправильно ввели ")
+        a = input("Введите первое число: ")
+
+b = input("Введите второе число: ")      #Обработка искоючений
+while type(b) != float:
+    try:
+        b = float(b)
+    except ValueError:
+        print("Неправильно ввели")
+        b = input("Введите второе число: ")
+
+
+action = input("Выберите операцию: (+ - * /) : ")
+if action == "+":
+    print(a + b)
+elif action == "-":
+    print(a - b)
+elif action == "*":
+    print(a * b)
+elif action == "/":
+    print(a / b)
